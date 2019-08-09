56f9e7f25b2fbc9b8f0e4bb8ca415a24e3ffafaa
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-08-09 18:37:19 +0700

    a

--- app/src/main/java/khoapham/ptp/phamtanphat/oop29072019/MainActivity.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/oop29072019/MainActivity.java
@@ -3,6 +3,7 @@ package khoapham.ptp.phamtanphat.oop29072019;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        show(1);
+
+    }
+    // phuong thức: method , function
+    //1 : Phạm vi hoạt động : access modifier
+    //2 : Giá trị trả về
+    //3 : Tên
+    private void show(Integer number){
+        //autoboxing
+        //unboxing
+        Log.d("BBB",number + "");
     }
 }
